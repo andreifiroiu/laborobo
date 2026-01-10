344548f80a7d9e73b4d96b24a0aab7436b0bb3ba
Andrei Firoiu <andrei.firoiu@gmail.com>
2026-01-10 14:07:55 +0200

    Add OpenTelemetry instrumentation for frontend and backend logging

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,5 +1,6 @@
 import '../css/app.css';
 import './lib/i18n';
+import './lib/telemetry'; // OpenTelemetry instrumentation
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
